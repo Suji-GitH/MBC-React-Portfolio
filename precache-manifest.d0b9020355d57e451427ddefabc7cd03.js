bbfb3c7a70d36b836a2ecae3cda888a06751e814
Sujith Ranjitkumar <sujithranjitk@gmail.com>
2020-09-29 17:14:29 +1000

    Updates

---
+++ precache-manifest.d0b9020355d57e451427ddefabc7cd03.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d0cd0b6db292f92fa4c960fb8089d15",
+    "url": "/MBC-React-Portfolio/index.html"
+  },
+  {
+    "revision": "62b6f982846d2e289751",
+    "url": "/MBC-React-Portfolio/static/css/main.6f85b78b.chunk.css"
+  },
+  {
+    "revision": "67c7d912ea5d8b9db6bb",
+    "url": "/MBC-React-Portfolio/static/js/2.d91ec0bc.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/MBC-React-Portfolio/static/js/2.d91ec0bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b6f982846d2e289751",
+    "url": "/MBC-React-Portfolio/static/js/main.d616b0f4.chunk.js"
+  },
+  {
+    "revision": "880c2abd1c409a96f433",
+    "url": "/MBC-React-Portfolio/static/js/runtime-main.2f9dd482.js"
+  },
+  {
+    "revision": "470e52e53cb22c6f852ad0a72d295217",
+    "url": "/MBC-React-Portfolio/static/media/ChatApp2.470e52e5.jpg"
+  },
+  {
+    "revision": "704a45f5ed1cb1b365c9573584875404",
+    "url": "/MBC-React-Portfolio/static/media/ET.704a45f5.jpg"
+  },
+  {
+    "revision": "e523adf65fe845f78d3e484f4455e6db",
+    "url": "/MBC-React-Portfolio/static/media/NT.e523adf6.jpg"
+  },
+  {
+    "revision": "92c9ea336f0005f33013a4343d4c82db",
+    "url": "/MBC-React-Portfolio/static/media/PFP.92c9ea33.jpg"
+  },
+  {
+    "revision": "67bef3ff976fc0b66f758c398ab508d0",
+    "url": "/MBC-React-Portfolio/static/media/PassGen2.67bef3ff.jpg"
+  },
+  {
+    "revision": "a0472eb08e628301ad0356c0afc98355",
+    "url": "/MBC-React-Portfolio/static/media/Resume.a0472eb0.pdf"
+  },
+  {
+    "revision": "93f9350c2597f4017a6afaac07244b4a",
+    "url": "/MBC-React-Portfolio/static/media/WeatherDash.93f9350c.jpg"
+  }
+]);
